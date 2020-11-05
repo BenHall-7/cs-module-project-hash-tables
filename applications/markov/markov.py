80c27c14bcae799a43eab54a8b19756c32dfcf11
BenHall-7 <benjaminjahall@gmail.com>
2020-11-05 01:30:22 -0500

    Do markov chain problem

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -1,13 +1,54 @@
 import random
 
+# {
+#   'test': ["a", "b", "c" ...],
+#    ...
+# }
+chains = {}
+start_words = []
+
+def is_start(word: str):
+    if word[0] == "\"":
+        return len(word) > 1 and word[1].isupper()
+    else:
+        return word[0].isupper()
+
+def is_stop(word: str):
+    if word[-1] == "\"":
+        return len(word) > 1 and word[-2] in [".", "?", "!"]
+    else:
+        return word[-1] in [".", "?", "!"]
+
 # Read in all the words in one go
-with open("input.txt") as f:
+with open("C:\\Users\\Breakfast\\Code\\Lambda\\cs_5\\cs-module-project-hash-tables\\applications\\markov\\input.txt") as f:
     words = f.read()
+    prev = None
+    for word in words.split(" "):
+        word = word.strip()
+        if prev:
+            if is_start(prev):
+                start_words.append(prev)
+            
+            new_entry = (word, is_stop(word))
+            if prev not in chains:
+                chains[prev] = [new_entry]
+            else:
+                chains[prev].append(new_entry)
 
-# TODO: analyze which words can follow other words
-# Your code here
+        prev = word
 
+# construct 5 random sentences
 
-# TODO: construct 5 random sentences
-# Your code here
+for _ in range(5):
+    first = random.choice(start_words)
+    print(first, end=" ")
+    if is_stop(first):
+        continue
+
+    cur = random.choice(chains[first])
+    while True:
+        print(cur[0], end=" ")
+        if cur[1]:
+            break
+        cur = random.choice(chains[cur[0]])
 
