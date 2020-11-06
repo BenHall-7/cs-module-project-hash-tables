57830f463bea7e6818398af01c8d3a9e2467d0b4
BenHall-7 <benjaminjahall@gmail.com>
2020-11-05 22:20:26 -0500

    Do histogram problem

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,12 @@
-# Your code here
+words = {}
+max_len = 0
 
+with open("C:\\Users\\Breakfast\\Code\\Lambda\\cs_5\\cs-module-project-hash-tables\\applications\\histo\\robin.txt") as f:
+    text = f.read()
+    for word in text.split():
+        word = "".join(filter(lambda ch: ch not in r"\":;,.-+=/\\|[]{}()*^&", word)).lower()
+        words[word] = 1 if not word in words else words[word] + 1
+        max_len = max(max_len, len(word))
+
+for word, num in sorted(sorted(words.items()), key=lambda x: x[1], reverse=True):
+    print(f"{word: <{max_len + 2}}", "#"*num)
